c2fe05b8f05f09d18e3f78878ee416c3623d11a7
AllenBertrand <bertrandallen@gmail.com>
2016-03-10 16:31:13 -0800

    Lab 6
    Doesn't fit on moodle anymore

---
+++ CommunityInfoSite/CommunityInfoSite/Controllers/AuthController.cs
@@ -0,0 +1,136 @@
+﻿using CommunityInfoSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using BookStoreDemo1.Models;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CommunityInfoSite.Controllers
+{
+    public class AuthController : Controller
+    {
+        //Instance of user manager for authcontroller
+        UserManager<MemberUser> userManager = new UserManager<MemberUser>(
+        new UserStore<MemberUser>(new CommunityForumContext()));
+
+
+        // GET: Auth/Login
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new LogInModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult LogIn(LogInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = userManager.Find(model.Email, model.Password);
+
+            if (user != null)
+            {
+                var identity = userManager.CreateIdentity(
+                    user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                GetAuthenticationManager().SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // user authentication failed
+            ModelState.AddModelError("", "Invalid email or password");
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = new MemberUser
+            {
+                UserName = model.Email,
+                ForumNick = model.ForumNick
+            };
+
+            var result = userManager.Create(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                SignIn(user);
+                return RedirectToAction("index", "home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+
+            return View();
+        }
+
+        private void SignIn(MemberUser user)
+        {
+            var identity = userManager.CreateIdentity(
+                user, DefaultAuthenticationTypes.ApplicationCookie);
+
+            GetAuthenticationManager().SignIn(identity);
+        }
+
+        private IAuthenticationManager GetAuthenticationManager()
+        {
+            var ctx = Request.GetOwinContext();
+            return ctx.Authentication;
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "home");
+        }
+
+        //asp.net cleanup method
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && userManager != null)
+            {
+                userManager.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
